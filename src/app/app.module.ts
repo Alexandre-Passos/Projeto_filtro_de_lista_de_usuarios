115ba04e1627630f20e44408a9732581c290ce74
alexandrepassos <alexandre15passos@gmail.com>
2025-09-12 07:29:43 -0300

    inicializando o projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ComponenteModule } from './componentes/componente/componente.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AngularMaterialModule } from './angular-material/angular-material/angular-material.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ComponenteModule,
+    BrowserAnimationsModule,
+    AngularMaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/componentes/componente/componente.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { AngularMaterialModule } from 'src/app/angular-material/angular-material/angular-material.module';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    AngularMaterialModule
+  ],
+  exports: []
+})
+export class ComponenteModule { }
