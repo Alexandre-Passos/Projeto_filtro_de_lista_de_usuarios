592faddda0d1401f4893a4b5fbf46920830e841e
alexandrepassos <alexandre15passos@gmail.com>
2025-09-13 08:57:02 -0300

    implementando o layout dos filtros

--- src/app/angular-material/angular-material.module.ts
+++ src/app/angular-material/angular-material.module.ts
@@ -1,17 +1,26 @@
 import { NgModule } from '@angular/core';
 
-import {MatListModule} from '@angular/material/list'; // Material para lista
-import {MatDividerModule} from '@angular/material/divider'; //Material para uma linha de dividir
+import { MatListModule } from '@angular/material/list'; // Material para lista
+import { MatDividerModule } from '@angular/material/divider'; //Material para uma linha de dividir
 
-import {MatFormFieldModule} from '@angular/material/form-field'; //Material do form
-import {MatInputModule} from '@angular/material/input';  //Material do Input
+import { MatFormFieldModule } from '@angular/material/form-field'; //Material do form
+import { MatInputModule } from '@angular/material/input';  //Material do Input
+
+
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+
+import { MatSelectModule } from '@angular/material/select';
+import { CommonModule } from '@angular/common';
+
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [],
 
-  imports: [MatListModule, MatDividerModule, MatFormFieldModule, MatInputModule],
+  imports: [MatListModule, MatDividerModule, MatFormFieldModule, MatInputModule, MatDatepickerModule, MatNativeDateModule, MatSelectModule, CommonModule, MatButtonModule],
 
-  exports: [MatListModule, MatDividerModule, MatFormFieldModule, MatInputModule]
+  exports: [MatListModule, MatDividerModule, MatFormFieldModule, MatInputModule, MatDatepickerModule, MatNativeDateModule, MatSelectModule, CommonModule, MatButtonModule]
 
 })
 export class AngularMaterialModule { }
